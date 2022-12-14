69f89083d2d95f05dff57e2947ffbb9cec8b82c6
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2022-12-14 21:33:19 +0530

    Todos layout added

---
+++ frontend/src/Components/TodoItem.js
@@ -0,0 +1,13 @@
+function TodoItem({ todo, onDelete }) {
+  return (
+    <div>
+      <h4>{todo.title}</h4>
+      <p>{todo.desc}</p>
+      <button className='btn' onClick={onDelete(todo)}>
+        Delete
+      </button>
+    </div>
+  )
+}
+
+export default TodoItem
