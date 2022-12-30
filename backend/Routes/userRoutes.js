863a2e62d502eacaf27f62df94ef808fc4315f9a
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2022-12-30 13:19:12 +0530

    modiefied code

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,7 +1,13 @@
+// Importing Express to define routes
 const express = require('express')
-const { home, aboutus, todoCreate } = require('../controllers/todoControllers')
 const router = express.Router()
+
+// Importing Controllers from todoController
+const { home, aboutus, todoCreate } = require('../controllers/todoControllers')
+
+//middleware
 router.get('/', home)
+
 router.get('/aboutus', aboutus)
 router.post('/todo', todoCreate)
 module.exports = router
