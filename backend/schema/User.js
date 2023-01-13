deefeadd3d8b9a678be2d93af2cf18e533f1d5c1
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2023-01-13 20:03:26 +0530

    encryption of password using bcrypt

--- backend/schema/User.js
+++ backend/schema/User.js
@@ -1,21 +1,41 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 const UserSchema = mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, 'Please add a name'],
+    maxlength: [50, 'Name cannot be more than 40 characters'],
   },
   email: {
     type: String,
-    required: true,
+    required: [true, 'Please add an email'],
+    validate: [validator.isEmail, 'Please Enter a valid email'],
     unique: true,
   },
   password: {
     type: String,
-    required: true,
+    required: [true, 'Please provide a password'],
+    minlength: [6, 'Password must be at least 6 characters'],
   },
-  date: {
+  forgotPasswordToken: String,
+  forgotPasswordExpire: Date,
+  createdAt: {
     type: Date,
     default: Date.now,
   },
 })
+// encrypting password using bcrypt before save
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next()
+  }
+  // this used to access any of the fields in the schema
+  this.password = await bcrypt.hash(this.password, 10)
+})
+
+// Validate the password with passed on user password
+//isValidatedPassword is a method that we are creating which will return true or false
+UserSchema.methods.isValidatedPassword = async function (usersendpassword) {
+  return await bcrypt.compare(usersendpassword, this.password)
+}
 module.exports = mongoose.model('user', UserSchema)
