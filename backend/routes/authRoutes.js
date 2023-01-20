6bc62959c2a7c3e369453eaf56c6288042a7a267
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2023-01-20 23:11:45 +0530

    added dashboard route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 // //Importing express-validator
-// const { body } = require('express-validator')
+const { body } = require('express-validator')
 
 //importing controllers from authController
 const { register, loginuser } = require('../controllers/userController')
@@ -11,17 +11,20 @@ const { register, loginuser } = require('../controllers/userController')
 //create a User using Post
 router.post(
   '/register',
-  // ,
-  // [
-  //   body('firstname', 'Please enter a valid first name').not().isEmpty(),
-  //   body('lastname', 'Please enter a valid last name').not().isEmpty(),
-  //   body('email', 'Please enter a valid email').isEmail(),
-  //   body('password', 'Password must be at least 6 characters').isLength({
-  //     min: 6,
-  //   }),
-  // ],
+  [
+    body('firstname', 'Please enter a valid first name').not().isEmpty(),
+    body('lastname', 'Please enter a valid last name').not().isEmpty(),
+    body('email', 'Please enter a valid email').isEmail(),
+    body('password', 'Password must be at least 6 characters').isLength({
+      min: 6,
+    }),
+  ],
   register
 )
 //login user using Post request
 router.post('/login', loginuser)
+
+//dashboard Route
+router.get('/dashboard', dashboard)
+
 module.exports = router
