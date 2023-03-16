1910511ce4a91e21b78ce78aa23ae85ff8c8a5de
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2023-03-16 22:27:04 +0530

    basic auth UI done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,49 +1,30 @@
 import { useState } from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Nav from './Components/Layouts/Nav'
 import Footer from './Components/Layouts/Footer'
-import Todos from './Components/Todos'
-import AddTodo from './Components/AddTodo'
-
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 function App() {
-  const onDelete = (todo) => {
-    console.log('I am on delete of todo', todo)
-    setTodos(
-      todos.filter((e) => {
-        return e !== todo
-      })
-    )
-  }
-  const [todos, setTodos] = useState([
-    {
-      sno: 1,
-      title: 'Go to the market',
-      task: 'You need to go to the market to get this job done 01',
-      task2: 'You need to go to the market to get this job done 01',
-    },
-    {
-      sno: 2,
-      title: 'Go to the mall',
-      task: 'You need to go to the mall to get this job done 02',
-    },
-    {
-      sno: 3,
-      title: 'Go to the shop',
-      task: 'You need to go to the shop to get this job done 03',
-    },
-  ])
-
   return (
-    <Router>
-      <div className='grid grid-col-3 content-between min-h-screen '>
-        <Nav />
-        <main className='sm:flex sm:flex-col-rev md:flex  justify-around'>
-          <Todos todos={todos} onDelete={onDelete} />
-          <AddTodo />
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <>
+      <Router>
+        <div className='grid grid-col-3 content-between min-h-screen '>
+          <Nav />
+          <main className='sm:flex sm:flex-col-rev md:flex  justify-around'>
+            <Routes>
+              <Route path='/' element={<Dashboard />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
   )
 }
 
--- frontend/src/Components/Layouts/Nav.js
+++ frontend/src/Components/Layouts/Nav.js
@@ -1,6 +1,19 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { FaSignOutAlt, FaUser } from 'react-icons/fa'
 import logo from './images/logo.png'
+import { useSelector, useDispatch } from 'react-redux'
+import { logout, reset } from '../../features/auth/authSlice'
+
 function Nav() {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { user } = useSelector((state) => state.auth)
+
+  const onLogout = () => {
+    dispatch(logout())
+    dispatch(reset())
+    navigate('/')
+  }
   return (
     <div className='navbar px-4 py-2 bg-base-100 '>
       <div className='flex-1'>
@@ -12,7 +25,7 @@ function Nav() {
           />
         </Link>
       </div>
-      <div className='flex-none gap-2'>
+      <div className='flex-none'>
         <div className='form-control'>
           <input
             type='text'
@@ -20,30 +33,29 @@ function Nav() {
             className='input input-bordered'
           />
         </div>
-        <div className='dropdown dropdown-end'>
-          <label tabIndex={0} className='btn btn-ghost btn-circle avatar'>
-            <div className='w-10 rounded-full'>
-              <img alt='no img' src='https://placeimg.com/80/80/people' />
-            </div>
-          </label>
-          <ul
-            tabIndex={0}
-            className='mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52'
-          >
+        <ul className='menu menu-horizontal px-2 gap-2'>
+          {user ? (
             <li>
-              <Link to='/' className='justify-between'>
-                Profile
-                <span className='badge'>New</span>
-              </Link>
+              <buton className='btn' onClick={onLogout}>
+                <FaSignOutAlt /> Logout
+              </buton>
             </li>
-            <li>
-              <Link href='/'>Settings</Link>
-            </li>
-            <li>
-              <Link to='/'>Logout</Link>
-            </li>
-          </ul>
-        </div>
+          ) : (
+            <>
+              <li>
+                <Link to='/login'>
+                  <FaSignOutAlt />
+                  Login
+                </Link>
+              </li>
+              <li>
+                <Link to='/register'>
+                  <FaUser /> Register
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
       </div>
     </div>
   )
