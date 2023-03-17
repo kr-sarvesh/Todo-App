799e5caa795b7ef5cb13661047ba3966f3e9bc74
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2023-03-18 00:57:17 +0530

    Added useEffect, useNavigate, useSelector hook

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,17 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
 function Dashboard() {
-  return <div>Dashboard</div>
+  const navigate = useNavigate()
+  const { user } = useSelector((state) => state.auth)
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/login')
+    }
+  }, [user, navigate])
+  return <>Dashboard</>
 }
 
 export default Dashboard
