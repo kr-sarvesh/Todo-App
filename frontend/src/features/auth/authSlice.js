f47fd9d75ad01443617fb00441e7f4644cabeaba
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2023-03-21 23:50:44 +0530

    updated code

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -59,6 +59,9 @@ export const authSlice = createSlice({
         state.message = action.payload
         state.user = null
       })
+      .addCase(logout.fulfilled, (state) => {
+        state.user = null
+      })
   },
 })
 export const { reset } = authSlice.actions
