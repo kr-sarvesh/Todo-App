2336c2155f40ef2506d4327557464c38aa5b414a
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2023-03-22 00:05:39 +0530

    added logout, login function

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -18,12 +18,24 @@ const register = async (userData, thunkAPI) => {
     return thunkAPI.rejectWithValue(message)
   }
 }
+
+// Login user
+const login = async (userData) => {
+  const response = await axios.post(`${API_URL}login`, userData)
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data))
+  }
+  return response.data
+}
+
 //logout user
 const logout = async () => {
   localStorage.removeItem('user')
 }
+
 const authService = {
   register,
+  login,
   logout,
 }
 
