35cd76c49181be8f0d2d59057b98e47961905943
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2023-03-21 23:58:15 +0530

    added login function

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -25,6 +25,18 @@ export const register = createAsyncThunk(
 )
 
 //Login user
+export const login = createAsyncThunk('auth/loginUser', async (user, thunkAPI) => {
+     try {
+     return await authService.login(user)
+     } catch (error) {
+          const message =
+          (error.response && error.response.data && error.response.data.message) ||
+          error.message ||
+          error.toString()
+          return thunkAPI.rejectWithValue(message)
+
+     }
+}
 
 //Logout user
 export const logout = createAsyncThunk('auth/logout', async () => {
