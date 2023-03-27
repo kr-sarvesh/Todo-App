2b379d0d3e4c31a6a44350ff88ea663e2890eab5
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2023-03-27 10:05:02 +0530

    removed  bugs

--- frontend/src/components/TodoForm.jsx
+++ frontend/src/components/TodoForm.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { createTodo, reset } from '../features/todo/todoSlice'
+import { createTodo, getAllTodos, reset } from '../features/todo/todoSlice'
 import { toast } from 'react-toastify'
 import Spinner from './Spinner'
 
@@ -11,21 +11,13 @@ function TodoForm() {
 
   const dispatch = useDispatch()
 
-  const { isLoading, isError, isSuccess, message } = useSelector(
-    (state) => state.todo
-  )
+  const { isLoading, isError, message } = useSelector((state) => state.todo)
 
   useEffect(() => {
     if (isError) {
-      toast.error(message)
+      console.log('There is error')
     }
-
-    if (isSuccess) {
-      toast.success(message)
-    }
-
-    dispatch(reset())
-  }, [isError, isSuccess, message, dispatch])
+  }, [isError])
 
   const onChange = (e) => {
     setTodoData((prevState) => ({
@@ -47,36 +39,38 @@ function TodoForm() {
     return <Spinner />
   }
   return (
-    <section className='form'>
-      <form onSubmit={onSubmit}>
-        <div className='form-group'>
-          <label htmlFor='title'>Todos</label>
-          <input
-            type='text'
-            placeholder='Enter title'
-            name='title'
-            id='title'
-            value={title}
-            onChange={onChange}
-            required
-          />
-          <input
-            type='text'
-            name='tasks'
-            placeholder='Enter tasks'
-            id='tasks'
-            value={tasks}
-            required
-            onChange={onChange}
-          />
-        </div>
-        <div className='form-group'>
-          <button className='btn btn-block' type='submit'>
-            Add Todo
-          </button>
-        </div>
-      </form>
-    </section>
+    <>
+      <section className='form'>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <label htmlFor='title'>Todos</label>
+            <input
+              type='text'
+              placeholder='Enter title'
+              name='title'
+              id='title'
+              value={title}
+              onChange={onChange}
+              required
+            />
+            <input
+              type='text'
+              name='tasks'
+              placeholder='Enter tasks'
+              id='tasks'
+              value={tasks}
+              required
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <button className='btn btn-block' type='submit'>
+              Add Todo
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
