d4d7d2e745b33db3eb153fdd999e9fc7b777cd6c
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2023-03-28 22:52:55 +0530

    added todoItem in dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,14 +2,17 @@ import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import TodoForm from '../components/TodoForm'
-import { getAllTodos, reset } from '../features/todo/todoSlice'
+import TodoItem from '../components/TodoItem'
+import { getAllTodos } from '../features/todo/todoSlice'
 import Spinner from '../components/Spinner'
 
 function Dashboard() {
   const navigate = useNavigate()
   const dispatch = useDispatch()
   const { user } = useSelector((state) => state.auth)
-  const { isError, message, isLoading } = useSelector((state) => state.todos)
+  const { isError, message, isLoading, todos } = useSelector(
+    (state) => state.todos
+  )
 
   useEffect(() => {
     if (isError) {
@@ -30,7 +33,18 @@ function Dashboard() {
       <section className='heading'>
         <h1>Welcome {user && user.name}</h1>
         <p>Todos Dashboard</p>
-        <TodoForm />
+      </section>
+      <TodoForm />
+      <section className='content'>
+        {todos.length > 0 ? (
+          <div className='todos'>
+            {todos.map((todo) => (
+              <todoItem key={todo._id} goal={todo} />
+            ))}
+          </div>
+        ) : (
+          <h3>You have not set any todos</h3>
+        )}
       </section>
     </>
   )
