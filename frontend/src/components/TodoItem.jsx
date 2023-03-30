d5a64c6f5cc506cec3442e886c9519cdf36516ae
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2023-03-30 16:10:58 +0530

    installed react modal

--- frontend/src/components/TodoItem.jsx
+++ frontend/src/components/TodoItem.jsx
@@ -1,46 +1,55 @@
+import { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { deleteTodo, updateTodo } from '../features/todo/todoSlice'
+import { deleteTodo, addTask } from '../features/todo/todoSlice'
 import { TiChevronRight } from 'react-icons/ti'
 import { RiTaskFill } from 'react-icons/ri'
 import { AiFillEdit } from 'react-icons/ai'
 import { AiOutlineDelete } from 'react-icons/ai'
+
 function TodoItem({ todo }) {
+  const [show, setShow] = useState(false)
+
   const dispatch = useDispatch()
 
   return (
-    <div className='todo'>
-      <div>{new Date(todo.createdAt).toLocaleString('en-US')}</div>
-      <h2>todo title is: {todo.title}</h2>
-      <div>
-        Todo tasks:
-        <div className='taskList'>
-          <ul>
-            {todo.tasks.map((task) => {
-              return (
-                <li>
-                  <TiChevronRight /> {task}
-                </li>
-              )
-            })}
-            <div className='todolisticons'>
-              <div className='taskFill'>
-                <RiTaskFill />
+    <>
+      <div className='todo'>
+        <div>{new Date(todo.createdAt).toLocaleString('en-US')}</div>
+        <h2>todo title is: {todo.title}</h2>
+        <div>
+          Todo tasks:
+          <div className='taskList'>
+            <ul>
+              {todo.tasks.map((task) => {
+                return (
+                  <li>
+                    <TiChevronRight /> {task}
+                  </li>
+                )
+              })}
+              <div className='todolisticons'>
+                <div className='taskFill'>
+                  <RiTaskFill onClick={(e) => dispatch(addTask(todo.id))} />
+                </div>
+                <div className='taskEdit'>
+                  <AiFillEdit />
+                </div>
+                <div className='taskDelete'>
+                  <AiOutlineDelete />
+                </div>
               </div>
-              <div className='taskEdit'>
-                <AiFillEdit />
-              </div>
-              <div className='taskDelete'>
-                <AiOutlineDelete />
-              </div>
-            </div>
-          </ul>
+            </ul>
+          </div>
         </div>
-      </div>
 
-      <button onClick={(e) => dispatch(deleteTodo(todo._id))} className='close'>
-        X
-      </button>
-    </div>
+        <button
+          onClick={(e) => dispatch(deleteTodo(todo._id))}
+          className='close'
+        >
+          X
+        </button>
+      </div>
+    </>
   )
 }
 
